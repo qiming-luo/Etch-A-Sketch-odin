18b4df3ace1672df6e2f650a548218a0aea33c08
luoqiming82 <lowuji22@gmail.com>
2023-02-25 09:48:01 +0800

    replane

--- module/rgbarandom.js
+++
@@ -1,7 +0,0 @@
-// creat random rgba number
-let r =Math.floor(Math.random()*256); // random 0-255
-let g = Math.floor(Math.random()*256);
-let b = Math.floor(Math.random()*256);
-let a = Math.random();
-
-//export {r,g,b,a};
