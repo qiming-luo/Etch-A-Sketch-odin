4cb3e0a0600c05739bacb2a5e8d19c76f52a56ec
luoqiming82 <lowuji22@gmail.com>
2023-02-25 11:55:57 +0800

    reconstruct the logic

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ const backgroundColorChange = (domElement) => {
 }
 
 
-// initial grid
+//
 
-let main = document.getElementById('main');
+let grid = document.getElementById('grid');
+let btn = document.getElementById('btn');
 
 
 // create grid container and style it 
@@ -32,37 +33,32 @@ for(let i = 1; i<= 16*16; i++){
     gridContainer.appendChild(box);
 }
 
-main.appendChild(gridContainer);
+grid.appendChild(gridContainer);
+
+
+for(let i =1; i<=16*16; i++){
+    gridContainer.removeChild(gridContainer.firstChild);
+}
 
 
 // create a function to build grid containner and its childs, handle background color changeing
 
 function gridCreator(num){
-    // remove old grid container
-    main.removeChild(gridContainer);
-    // create grid container and style it 
-    let gridContainer = document.createElement('div');
-    gridContainer.style.width = '1000px';
-    gridContainer.style.aspectRatio = '1/1';
-    gridContainer.style.border = '1px solid green';
-    gridContainer.style.margin = '2rem auto';  // make it center horizonly
-    gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateColumns = 'repeat(num, 1fr)';
-    gridContainer.style.gridTemplateRows = 'repeat(num,1fr)';
-    gridContainer.style.gap = '0.1rem';
-    //
-    //creat children grids and append them to grid container
-    for(let i = 1; i<= num*num; i++){
-        let box = document.createElement('div');
-        box.style.backgroundColor = 'lightGreen';
-        // hover, change background color
-        box.addEventListener('mouseover',function(){backgroundColorChange(box)}); //When passing parameter values, use an "anonymous function" that calls the specified function with the parameters
-        box.innerHTML = `${i}`;
-        gridContainer.appendChild(box);
-    }
-    //
-    //
-    main.appendChild(gridContainer);
+    
+    
 }
 
+/*
 // add event listener to handle user input and creat new grid as user wish
+const btnHandler = () => {
+    
+    let userInput = prompt('Please enter a number between 2 and 20');
+    if(userInput>1 && userInput<21){
+        gridCreator(userInput);
+    }
+    
+   // gridCreator(10);
+}
+
+btn.addEventListener('click', btnHandler);
+*/
